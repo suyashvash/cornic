f2abd7d7fffbe2981976f270bd5f8e1c2eec052e
SUYASH VASHISHTHA <suyashvashishtha@gmail.com>
2021-09-25 00:59:58 +0530

    nav bugs fixed

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -6,7 +6,7 @@ import logo from '../assets/logo.png'
 import { FaPlus } from 'react-icons/fa'
 import { selectLoggedIN } from "../features/userSlice";
 import { useSelector } from "react-redux";
-
+import { BsInfoCircleFill } from 'react-icons/bs';
 import { AiFillHome } from 'react-icons//ai'
 
 import { Link } from 'react-router-dom'
@@ -66,7 +66,7 @@ export default function NavBar() {
                     </Nav.Item>
 
                     <Nav.Item className="ml-auto">
-                        <Link className="ask-a-ques" to={{ pathname: "/cornic-about" }}><BsInfoCircleFill size={20} color={'white'} /></Link>
+                        <Link className="ask-a-ques" to={{ pathname: "/about" }}><BsInfoCircleFill size={20} color={'white'} /></Link>
                     </Nav.Item>
 
                 </Nav>
